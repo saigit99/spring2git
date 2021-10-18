e341cc27bf68d5799ee81436373b8874fdb2ba4f
saigit99 <87813128+saigit99@users.noreply.github.com>
2021-10-19 00:17:10 +0530

    first commit
    data copied

---
+++ src/test/java/com/example/demo/Azure2azureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Azure2azureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
